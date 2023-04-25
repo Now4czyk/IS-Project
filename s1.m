96b7401c6ff31ac05b9a89275ff97cd29776dd6f
Kacper Nowaczyk <kacpern30@gmail.com>
2023-04-25 20:57:07 +0200

    hanning window with bode diagrams implementation

--- s1.m
+++
@@ -1,9 +0,0 @@
-% Załadowanie oraz wybranie kluczowych do identyfikacji danych
-load('cstr.dat');
-data=cstr(:, 3);
-dataSize = size(data);
-
-% Ilość próbek
-N = dataSize(1);
-t = 0:1:N-1; 
-plot(t, data)
